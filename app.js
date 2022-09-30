84e5ba89b41cfa879d3696799cf1a4453d8a28ef
Namiq <107476714+namiqmamedov@users.noreply.github.com>
2022-09-30 05:56:08 +0400

    feat:Create slider & countdown time

--- app.js
+++ app.js
@@ -36,35 +36,88 @@ function myFunction1() {
 
 
 
+// dropdown
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+
+}
+
+
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show1");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn1')) {
+  var myDropdown1 = document.getElementById("myDropdown1");
+    if (myDropdown1.classList.contains('show1')) {
+      myDropdown1.classList.remove('show1');
+    }
+  }
+}
+$('dropdown-content').on('click', function(event){
+  
+  event.stopPropagation();
+});
+
+
+
 
 $(".slider").slick({
 
-  // normal options...
-  infinite: false,
-  dots: true,
+// normal options...
+infinite: false,
+dots: true,
+// autoplay: true,
+// autoplaySpeed: 1500,
 
-  // the magic
-  responsive: [{
+// the magic
+responsive: [{
 
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        infinite: true
-      }
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      infinite: true
+    }
 
-    }, {
+  }, {
 
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      dots: true
+    }
 
-    }, {
+  }, {
 
-      breakpoint: 300,
-      settings: "unslick" // destroys slick
+    breakpoint: 300,
+    settings: "unslick" // destroys slick
 
-    }]
+  }]
 });
 
+
+AOS.init();
+
+
+AOS.init({
+  once: true,
+  easing: 'ease-in-out'
+});
+var iconButton = $("button");
+iconButton.on("click", function() {
+  $(iconButton).removeClass('aos-animate');
+  setTimeout(function() {
+      $(iconButton).addClass('aos-animate');
+  }, 400);
+});
